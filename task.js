c1ce5723e9d390fb36a4f740c966b21750e26a9a
YuliyaPriymak <bogatir.yuliya@gmail.com>
2021-03-21 01:39:03 +0200

    add three cycle, uniqArr

--- task.js
+++ task.js
@@ -1,28 +1,32 @@
 let str = 'lambda';
 let arr = [];
-let strArr = str.split('');
 
+arr.push(str.split('').join('.'))
 
-// let newStr = (array) => {
-//     arr.push(array.join('.'))
-//     console.log(arr);
-//     return arr;
-// }
-
-// let xxx = (a) => {
-//     let start = str.slice(0, a)+'.';
-//     let end = str.slice(a, str.length);
-//     let all = start+end;
-//     arr.push(all)
-//     console.log(arr);
-//     return arr
-// }
-for (let a=0; a<=str.length; a++){
-    let start = str.slice(0, a)+'.';
+for (let a = 1; a < str.length; a++) {
+    let start = str.slice(0, a) + '.';
     let end = str.slice(a, str.length);
-    let all = start+end;
-    arr.push(all)
+    let sum = start + end;
+    arr.push(sum)
 }
-console.log(arr);
+
+
+for (let i = 2; i < str.length; i++) {
+    let start = str.slice(0, i).split('').join('.');
+    let end = str.slice(i, str.length);
+    let sum = start + end
+    arr.push(sum)
+}
+
+for (let i = str.length - 1; i > 1; i--) {
+    let end = str.slice(str.length - i).split('').join('.')
+    let start = str.slice(0, str.length - i);
+    let sum = start + end
+    arr.push(sum)
+}
+
+console.log(arr, arr.length);
+let uniqArr = [...new Set(arr.map(a => a))]
+console.log(uniqArr, uniqArr.length);
 
 
